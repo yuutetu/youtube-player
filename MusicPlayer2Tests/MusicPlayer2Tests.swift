3e2f0fe4cbeb6b9b296f683453c7272112227af2
yuutetu <yuutetu@example.com>
2016-12-23 01:02:55 +0900

    Initial Commit

---
+++ MusicPlayer2Tests/MusicPlayer2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MusicPlayer2Tests.swift
+//  MusicPlayer2Tests
+//
+//  Created by 加賀江　優幸 on 2016/12/23.
+//  Copyright © 2016年 Masayuki Kagae. All rights reserved.
+//
+
+import XCTest
+@testable import MusicPlayer2
+
+class MusicPlayer2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
