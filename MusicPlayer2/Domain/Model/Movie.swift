a0022b5fe045bb6fb8f0b047d5acba53cb4e6e49
yuutetu <yuutetu@example.com>
2017-02-12 00:28:09 +0900

    フォルダ構造を整理

---
+++ MusicPlayer2/Domain/Model/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MusicPlayer2
+//
+//  Created by 加賀江　優幸 on 2017/02/12.
+//  Copyright © 2017年 Masayuki Kagae. All rights reserved.
+//
+
+import Decodable
+
+struct Movie {
+    let videoId: String?
+    let title: String
+    let thumbnail: Thumbnail
+}
---
+++ MusicPlayer2/Domain/Model/Thumbnail.swift
@@ -0,0 +1,15 @@
+//
+//  Thumbnail.swift
+//  MusicPlayer2
+//
+//  Created by 加賀江　優幸 on 2017/02/12.
+//  Copyright © 2017年 Masayuki Kagae. All rights reserved.
+//
+
+import Foundation
+
+struct Thumbnail {
+    let url: URL
+    let width: Int
+    let height: Int
+}
